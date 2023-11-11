95e784b8ee181bde1dbb972e782bd04fe89b1a1b
LENG <leng@LENGs-MacBook-Pro.local>
2023-11-11 15:43:23 +0700

    new

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 import express from "express";
 import path from "path";
-import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
-// import { Server } from 'socket.io'; // Import the Server class
 import dotenv from "dotenv";
 import dialogflowRoutes from "./routes/dialogflow.js";
-// import notificationsRoutes from "./routes/notification.js";
 
 dotenv.config();
 
@@ -27,42 +24,15 @@ db.once("open", () => {
   console.log("Connected to MongoDB");
 });
 
-// Setup Socket.IO
-// const server = app.listen(port, () => {
-//   console.log(`Server Running at ${port}`);
-// });
-
-// // Create a Socket.IO instance and pass the existing server with CORS configuration
-// const io = new Server(server, {
-//   cors: {
-//     origin: "http://localhost:5173", // This should match the origin of your frontend
-//     methods: ["GET", "POST"],
-//   },
-// });
-// app.use((req, res, next) => {
-//   req.io = io;
-//   next();
-// });
-
-// io.on("connection", (socket) => {
-//   console.log("Connected to Socket.IO");
-
-//   socket.on("orderStored", (data) => {
-//     console.log("Product stored:", data.message);
-//     io.emit("orderStored", data); // Broadcast the event to all connected clients
-//   });
-// });
-
-// Configure body parser
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+// Configure body parser (if you need it for other middleware)
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Enable CORS for Express
-app.use(cors);
+app.use(cors());
 
 // Define routes
 app.use("/api/dialogflow", dialogflowRoutes);
-// app.use("/api/notifications", notificationsRoutes);
 
 // Serve static files for production
 if (process.env.NODE_ENV === "production") {
@@ -72,5 +42,10 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server Running at ${port}`);
+});
 
 export default app;
